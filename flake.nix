10e2a691f146fca64fac538afd2c2c3b0652166b
Neil Alen <neilaspace@outlook.com>
2026-06-06 14:42:49 +0800

    ♻️ 重新添加autoPatchelf

--- flake.nix
+++ flake.nix
@@ -32,7 +32,49 @@
 
                     # Tools needed during the build process itself
                     nativeBuildInputs = with pkgs; [
+                        autoPatchelfHook # Automatically patches ELF binaries/libraries
                         makeWrapper # Creates wrapper scripts
+                    ];
+
+                    # Libraries needed for autoPatchelfHook to find and link against
+                    buildInputs = with pkgs; [
+                        stdenv.cc.cc.lib # Essential C++ runtime
+                        alsa-lib # Audio
+                        atk # Accessibility
+                        cairo # Graphics
+                        cups # Needeed for electron
+                        dbus # Inter-process communication
+                        expat # XML parsing
+                        fontconfig # Font management
+                        freetype # Font rendering
+                        gdk-pixbuf # Image loading
+                        glib # Core libraries
+                        gobject-introspection # Object system introspection
+                        gtk3 # GUI Toolkit
+                        hicolor-icon-theme # Standard icon theme infrastructure
+                        libdrm # Direct Rendering Manager
+                        libGL # OpenGL
+                        libglvnd # OpenGL vendor-neutral dispatch
+                        mesa # OpenGL implementation
+                        nspr # Netscape Portable Runtime
+                        nss # Network Security Services
+                        pango # Text layout
+                        udev # Device management
+                        vulkan-loader # Vulkan support
+                        libX11 # X11 core
+                        libXcomposite # X11 compositing
+                        libXcursor # X11 cursors
+                        libXdamage # X11 damage reporting
+                        libXext # X11 extensions
+                        libXfixes # X11 fixes extension
+                        libXi # X11 input extension
+                        libXrandr # X11 RandR extension (screen config)
+                        libXrender # X11 rendering extension
+                        libXScrnSaver # X11 screen saver extension
+                        libxshmfence # X11 shared memory fences
+                        libXtst # X11 test extension (automation, etc.)
+                        libxcb # X protocol C binding
+                        libXxf86vm # XFree86 Video Mode extension
                     ];
 
                     installPhase = ''
@@ -79,6 +121,10 @@
                         runHook postInstall
                     '';
 
+                    installCheckPhase = ''
+                        # Check if the binary links correctly
+                        ldd $out/bin/xmcl | grep "not found" && exit 1 || exit 0
+                    '';
                     meta = with pkgs.lib; {
                         description = "X Minecraft Launcher (XMCL)";
                         homepage = "https://github.com/Voxelum/x-minecraft-launcher";
